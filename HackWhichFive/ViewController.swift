0f2994edcffa24b6b6b5d257f2fba913191e5a6d
mhortillano <mhortillano@github.com>
2018-02-26 10:52:30 -1000

    Created Contacts

--- HackWhichFive/ViewController.swift
+++ HackWhichFive/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+//Define an array variable using an array literal with5 items of type string
+        
+    @IBOutlet var TableView: UITableView!
+    var nameArray = ["Manolo", "Mokuba", "Gale", "Mimz", "Cy"]
+      
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+          // self.tableView = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+        {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      //set up cell to display items in nameArray
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+        let text = nameArray[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+    }
 }
 
